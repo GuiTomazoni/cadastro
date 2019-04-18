420b2b730843cb705db52b10585f4b06c1ac70de
Tecnico - TI <tecnico@epfserver.com.br>
2019-04-17 22:02:49 -0300

    arrumar o metodo de capitalizar

--- src/main/java/com/fundatec/lpi/camadas/services/CapitalizaNome.java
+++ src/main/java/com/fundatec/lpi/camadas/services/CapitalizaNome.java
@@ -3,9 +3,16 @@ package com.fundatec.lpi.camadas.services;
 public class CapitalizaNome implements TransformadorNome{
 
 	public String transforma(String nome) {
-		nome = Character.toUpperCase(nome.charAt(0)) + nome.substring(1);
+		for(int i = 0; i > nome.length(); i++) {
+			nome.charAt(i);
+			if(i == 0) {
+				nome.toUpperCase();
+			}
+			
+		}
 		return nome;
 	}
 	
 
 }
+//arrumar meu metodo
